623a5e28cbf13a1a2a0a0991c2274d1ace4aab69
W4llnut <111294521+W4llnut@users.noreply.github.com>
2022-08-22 21:24:25 +0200

    Add files via upload

---
+++ agent.py
@@ -0,0 +1,196 @@
+from numpy import arctan
+from ta.trend import *
+from ta.momentum import *
+from ta.volatility import *
+from algorithmETH import AlgorithmETH
+from time import time
+import requests
+import urllib.parse
+import hashlib
+import hmac
+import base64
+from json import dumps,loads
+from time import sleep, time
+import pandas as pd
+import os
+
+class AGENT:
+	def __init__(self):
+		# dati api
+		self.api_url = "https://api.kraken.com"
+		self.api_key = os.environ['API_KEY']
+		self.api_sec = os.environ['API_SEC']
+
+		# parametri
+		self.tassa = 0.0054
+		self.moltiplicatore = 5
+		self.invest = 1 # 100%
+
+		# algoritmi
+		self.ETH = AlgorithmETH(self.tassa,self.moltiplicatore)
+		self.A = [self.ETH]
+
+		# Parametri della simulazione
+		self.staticMoney = 95
+		self.staticETH = 0.03703
+
+		self.strategia = "-"
+		self.current = -1
+		self.currentName = ["ETH"]
+		self.currentNameResult = ["XETH"]
+
+		self.money = 0
+		self.stocks = 0
+		self.get_balance()
+		self.dentro = False
+		self.entrata = 0
+		self.ora = 0
+
+	# ========================= funzioni di gestione ========================= #
+	def buy(self, now, data, forced=False, which=-1):
+		self.A[0].df = data[0].astype(float)
+		self.A[0].analyzeDf()
+		if (not self.dentro and self.A[0].check_buy(-1) == True):
+			self.current = 0
+			self.ora = time()-60
+			self.get_balance()
+			spesa = self.invest*self.money
+			output = self.buy_order(0)
+
+			k = 0
+			while k<6:
+				sleep(10)
+				flag,costo,tassa,price,volume = self.get_trade_history(ora)
+				if flag:
+					self.entrata = price
+					break
+				k += 1
+
+			self.dentro = True
+			self.get_balance()
+			return [True,f"Buy: Crypto:{self.stocks} {self.currentName[self.current]}({costo}*{self.moltiplicatore}={costo*self.moltiplicatore}€) / Balance:{self.money}€ || {output}"]
+		elif forced:
+			self.current = which
+			self.ora = time()-60
+			self.get_balance()
+			spesa = self.invest*self.money
+			output = self.buy_order(0)
+
+			print(output)
+			while True:
+				sleep(10)
+				flag,costo,tassa,price,volume = self.get_trade_history(ora)
+				print(flag,costo,tassa,price)
+				if flag:
+					self.entrata = price
+					break
+
+			self.dentro = True
+			self.get_balance()
+			return [True,f"Buy: Crypto:{self.stocks} {self.currentName[self.current]}({costo}*{self.moltiplicatore}={costo*self.moltiplicatore}€) / Balance:{self.money}€ || {output}"]
+		return [False,""]
+
+	def sell(self, now, data, forced=False):
+		self.A[0].df = data[0].astype(float)
+		self.A[0].analyzeDf()
+		if (self.dentro and self.A[self.current].check_sell(-1, self.entrata) == True) or forced:
+			self.dentro = False
+			output = self.sell_order(0)
+
+			sleep(10)
+			self.get_balance()
+
+			m = self.current
+			self.current = -1
+			return [True,f"Sell: Crypto:{self.stocks} {self.currentName[m]} / Balance:{round(self.money,2)}€ || {output}"]
+		return [False,""]
+
+	def get_total_balance(self):
+		self.get_balance()
+		return f"Balance: {self.money}€ / Crypto: {self.stocks} {self.currentName[self.current]}({self.get_price()*self.stocks}€)"
+
+
+	def get_current_state(self, data):
+		self.A[0].df = data[0].astype(float)
+		self.A[0].analyzeDf()
+		return f"{self.currentName[0]}: EMAb={round(self.A[0].df[f'EMA{self.A[0].periodiB}'][-1],2)} / EMAl={round(self.A[0].df[f'EMA{self.A[0].periodiL}'][-1],2)} / Psar>={self.A[0].df['psar_di'][-1]} / ADX={round(self.A[0].df['adx'][-1],2)} / Aroon={round(self.A[0].df['aroon_indicator'][-1],2)} / Bollinger={self.A[0].df['bollinger_pband'][-1]}"
+
+
+	# ========================= funzioni di comunicazione ========================= #
+	def get_kraken_signature(self, urlpath, data, secret):
+		postdata = urllib.parse.urlencode(data)
+		encoded = (str(data['nonce']) + postdata).encode()
+		message = urlpath.encode() + hashlib.sha256(encoded).digest()
+
+		mac = hmac.new(base64.b64decode(secret), message, hashlib.sha512)
+		sigdigest = base64.b64encode(mac.digest())
+		return sigdigest.decode()
+
+	def kraken_request(self, url_path, data):
+		headers = {"API-Key": self.api_key, "API-Sign": self.get_kraken_signature(url_path, data, self.api_sec)}
+		resp = requests.post((self.api_url + url_path), headers=headers, data=data)
+		return resp
+
+	def get_price(self):
+		return float(requests.get(f'https://api.kraken.com/0/public/Ticker?pair={self.currentName[self.current]}EUR').json()['result'][f'{self.currentNameResult[self.current]}ZEUR']['a'][0])
+
+
+
+	# ========================= funzioni di richiesta ========================= #
+	def get_balance(self):
+		data = {"nonce":str(int(1000*time()))}
+		resp = self.kraken_request("/0/private/Balance", data)
+		data = resp.json()["result"]
+		money = 0
+		stocks= 0
+		if "ZEUR" in data:
+			money = float(data["ZEUR"])-self.staticMoney
+		if self.currentNameResult[0] in data:
+			stocks = float(data[self.currentNameResult[0]])-self.staticETH
+		self.money = money
+		self.stocks = stocks
+		return money,stocks
+
+	def get_trade_history(self, ora):
+		data = {"nonce": str(int(1000*time())),"trades": True, "start": ora}
+		resp = self.kraken_request('/0/private/TradesHistory', data)
+		if resp.json()["result"]["count"]>0:
+			v = []
+			dic = {}
+			for i in resp.json()["result"]["trades"]:
+				v.append(resp.json()["result"]["trades"][i])
+			dic["0"] = v
+			dic = dumps(dic)
+			df = pd.DataFrame.from_dict(loads(dic)["0"])
+			df = df.set_index("time").sort_index(ascending=False)
+			costo = df["cost"].astype(float).sum()
+			tassa = df["fee"].astype(float).sum()
+			volume = df["vol"].astype(float).sum()
+			price = df["fee"].astype(float).mean()
+			return True,costo,tassa,price,volume
+		else:
+			return False,0,0,0,0
+
+	def get_volume(self):
+		flag,costo,tassa,price,volume = self.get_trade_history(time()-600)
+		return volume
+
+	def buy_order(self, asset):
+		print(f"{self.currentNameResult[asset]}EUR")
+		volume = self.money/self.get_price()
+		price = self.get_price()+0.01
+		print(">",volume,price)
+		data = {"nonce": str(int(1000*time())),"ordertype": "limit","type": "buy","volume": volume,"pair": f"{self.currentName[asset]}EUR", "price": price, "leverage": self.moltiplicatore, "expiretm": 60}
+		resp = self.kraken_request('/0/private/AddOrder', data)
+		return dumps(resp.json())
+
+	def sell_order(self, asset):
+		print(f"{self.currentNameResult[asset]}EUR")
+		self.get_balance()
+		volume = self.get_volume()
+		price = self.get_price()
+		print(">",volume,price)
+		data = {"nonce": str(int(1000*time())),"ordertype": "limit","type": "sell","volume": volume,"pair": f"{self.currentName[asset]}EUR","price": price, "leverage": self.moltiplicatore}
+		resp = self.kraken_request('/0/private/AddOrder', data)
+		print(">>",resp.json())
+		return dumps(resp.json())
---
+++ algorithmETH.py
@@ -0,0 +1,118 @@
+from numpy import linspace, meshgrid, array, arctan
+import matplotlib.pyplot as plt
+from ta.trend import *
+from ta.momentum import *
+from ta.volatility import *
+import pandas as pd
+
+class AlgorithmETH:
+	def __init__(self, tassa, moltiplicatore):
+		self.df = -1
+
+		# parametri
+		self.tassa = tassa
+		self.moltiplicatore = moltiplicatore
+
+		# stop calls
+		self.stopWinMACD = self.tassa+0.4/self.moltiplicatore
+		self.stopLossMACD = (0.04)/self.moltiplicatore
+		self.stopWinMACDs = self.tassa+0.4/self.moltiplicatore
+		self.stopLossMACDs = (0.02)/self.moltiplicatore
+		self.stopWinBollinger = self.tassa+0.4/self.moltiplicatore
+		self.stopLossBollinger = (0.01)/self.moltiplicatore
+
+		# parametri periodi
+		self.ADXperiodo = 14
+		self.periodiB = 6
+		self.periodiL = 66
+		self.Periodo = 25
+		self.longPeriod = 3*self.Periodo
+
+		self.Breve = self.periodiB
+		self.Lunga = self.periodiL
+
+		self.strategia = "-"
+
+
+	# ========================= funzioni dell'algoritmo ========================= #
+	def check_buy(self, t):
+		macd = self.df[f'EMA{self.Breve}'][t]>self.df[f'EMA{self.Lunga}'][t]
+		rocMACD = 0.3<self.df['rocM'][t] and self.df['rocBreve'][t]<1.5
+		aroonMACD = self.df['aroon_indicator'][t]>50
+		sarM = self.df['psar_di'][t]==False
+		bollingerMACD = self.df['bollinger_wband'][t] > 1.5
+		
+		Smacd = self.df[f'EMA{self.Breve}'][t]<self.df[f'EMA{self.Lunga}'][t]
+		SrocMACD = -2<self.df['rocM'][t]<0.3 and self.df['rocLungo'][t]<-0.5 and -2<self.df['rocBreve'][t]<-0.3
+		SaroonMACD = self.df['aroon_indicator'][t]<-40
+		Sbollinger = self.df['bollinger_pband'][t-10]-self.df['bollinger_pband'][t]<0.1 and self.df['bollinger_pband'][t]>0.3
+		SsarM = self.df['psar_di'][t]==True
+		
+		if macd and rocMACD and aroonMACD:
+			adx = self.df['adx'][t]>40
+			if sarM and adx:
+				self.strategia = "MACD"
+		elif Smacd and SrocMACD and SaroonMACD and Sbollinger and False:
+			adx = self.df['adx'][t]>20
+			if SsarM and adx:
+				self.short = True
+				self.strategia = "MACDshort"
+		return self.strategia != "-"
+
+	def check_sell(self, t, entrata):
+		if self.strategia == "MACD":
+			if self.df[f'EMA{self.Breve}'][t]<self.df[f'EMA{self.Lunga}'][t] or self.stopCallMacd(t,entrata):
+				self.strategia = "-"
+		elif self.strategia == "MACDshort":
+			if self.df[f'EMA{self.Breve}'][t]>self.df[f'EMA{self.Lunga}'][t] or self.stopCallMacdshort(t,entrata):
+				self.strategia = "-"
+		return self.strategia == "-"
+
+	def stopCallMacd(self, t, entrata):
+		sar = self.moltiplicatore*(self.df['Close'][t]*(1-self.tassa)-entrata)/entrata>=0 and self.df['psar_di'][t]==True
+		upper = self.df['Close'][t]>entrata*(1+self.stopWinMACD)#*(1+self.df['atr_perc'][t]))
+		lower = self.df['Close'][t]<entrata*(1-self.stopLossMACD)#*(1+self.df['atr_perc'][t]))
+		return upper or lower or sar
+
+	def stopCallMacdshort(self, t, entrata):
+		sar = self.moltiplicatore*(self.df['Close'][t]*(1+self.tassa/2)-entrata*(1-self.tassa/2))/entrata<=0 and self.df['psar_di'][t]==False
+		lower = self.df['Close'][t]<entrata*(1-self.stopWinMACDs)#*(1+self.df['atr_perc'][t]))
+		upper = self.df['Close'][t]>entrata*(1+self.stopLossMACDs)#*(1+self.df['atr_perc'][t]))
+		return upper or lower or sar
+
+	def analyzeDf(self):
+		# EMA
+		self.df[f'EMA{self.periodiB}'] = ema_indicator(self.df['Close'], self.periodiB, False)
+		self.df[f'EMA{self.periodiL}'] = ema_indicator(self.df['Close'], self.periodiL, False)
+		macd = MACD(self.df['Close'])
+		self.df['macd'] = macd.macd()
+		self.df['signal'] = macd.macd_signal()
+
+		# ADX
+		adxI = ADXIndicator(self.df['High'],self.df['Low'],self.df['Close'], self.ADXperiodo, False)
+		self.df['pos_directional_indicator'] = adxI.adx_pos()
+		self.df['neg_directional_indicator'] = adxI.adx_neg()
+		self.df['adx'] = adxI.adx()
+
+		# Bollinger Bands
+		bollinger = BollingerBands(self.df['Close'],200)
+		self.df['bollinger_wband'] = bollinger.bollinger_wband()
+		self.df['bollinger_pband'] = bollinger.bollinger_pband()
+
+		# Parabolic SAR
+		parabolicSar = PSARIndicator(self.df['High'],self.df['Low'],self.df['Close'])
+		self.df['psar'] = parabolicSar.psar()
+		self.df['psar_di'] = self.df['psar']>self.df['Close']
+
+		# Aroon
+		aroon = AroonIndicator(self.df['Close'])
+		self.df['aroon_indicator'] = aroon.aroon_indicator()
+
+		# roc
+		rocM = ROCIndicator(self.df['Close'])
+		self.df['rocM'] = rocM.roc()
+		rocBreve = ROCIndicator(self.df['Close'],2)
+		self.df['rocBreve'] = rocBreve.roc()
+		rocLungo = ROCIndicator(self.df['Close'],95)
+		self.df['rocLungo'] = rocLungo.roc()
+		
